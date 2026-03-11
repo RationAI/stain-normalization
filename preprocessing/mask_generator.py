89c3d5a3528762c85337211e98198ec9f7774e2c
Adam Lopatka <524733@mail.muni.cz>
2026-03-11 06:17:32 +0100

    feat: add preprocessing

---
+++ preprocessing/mask_generator.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import pyvips
+import ray
+from openslide import PROPERTY_NAME_MPP_X, PROPERTY_NAME_MPP_Y, OpenSlide
+from rationai.masks import process_items, tissue_mask, write_big_tiff
+
+
+# folders:
+# archive tumor cases
+# chive negative cases
+# Prospective negative cases
+# Prospective test cases
+# Prospective tumor cases
+
+SLIDES_PATH = "/mnt/data/MOU/prostate/tile_level_annotations/"
+MASK_DEST = "./mask/tissue_masks"
+LEVEL = 3
+
+
+@ray.remote
+def process_slide(slide_path: Path) -> None:
+    # pyvips.Image xres and yres variables don't respect level therefore we
+    # extract proper spatial resulution based on desired level using OpenSlide
+    with OpenSlide(slide_path) as slide:
+        downsample = slide.level_downsamples[LEVEL]
+        xres = 1000 / (float(slide.properties[PROPERTY_NAME_MPP_X]) * downsample)
+        yres = 1000 / (float(slide.properties[PROPERTY_NAME_MPP_Y]) * downsample)
+
+    slide = pyvips.Image.new_from_file(slide_path, level=LEVEL)
+    mask = tissue_mask(slide, xres)
+    mask_path = Path(MASK_DEST, f"{Path(slide_path).stem}.tiff")
+    mask_path.parent.mkdir(exist_ok=True, parents=True)
+    write_big_tiff(mask, path=mask_path, mpp_x=xres, mpp_y=yres)
+
+
+def main() -> None:
+    slides = list(Path(SLIDES_PATH).rglob("*.mrxs"))
+    # process_slide(slides[0])
+    process_items(slides, process_item=process_slide)
+
+
+if __name__ == "__main__":
+    main()
---
+++ preprocessing/tiler.py
@@ -0,0 +1,111 @@
+from pathlib import Path
+
+import mlflow
+import ray
+from rationai.tiling import tiling
+from rationai.tiling.modules.masks import PyvipsMask
+from rationai.tiling.modules.tile_sources import OpenSlideTileSource
+from rationai.tiling.typing import TiledSlideMetadata, TileMetadata
+from rationai.tiling.writers import save_mlflow_dataset
+from sklearn.model_selection import train_test_split
+
+
+SLIDES_PATH = "/mnt/data/MOU/prostate/tile_level_annotations/"
+TISSUE_MASKS_PATH = "./mask/tissue_masks"
+TISSUE_MASKS_PATH = "/home/jovyan/staining/demo_data/masks"
+
+
+# level avg_mpp_x   avg_mpp_y
+# 0     0.233876    0.234331
+# 1     0.467751    0.468661
+# 2     0.935503    0.937323
+# 3     1.871006    1.874646
+# 4     3.742012    3.749291
+# 5     7.484024    7.498583
+# 6     14.968047   14.997165
+# 7     29.936095   29.994331
+# 8     59.872189   59.988661
+# 9     119.744379  119.97732
+SlideMPP = 0.23
+
+source = OpenSlideTileSource(mpp=SlideMPP, tile_extent=512, stride=256)
+
+
+MIN_TISSUE_PERCENTAGE = 0.0
+
+
+class TissueMask(PyvipsMask[TileMetadata]):
+    def forward_tile(
+        self, tile_labels: TileMetadata, class_overlaps: dict[int, float]
+    ) -> TileMetadata | None:
+        tissue = 1.0 - class_overlaps.get(0, 0)
+        if tissue <= MIN_TISSUE_PERCENTAGE:
+            return None
+        return tile_labels
+
+
+tissue_mask = TissueMask(
+    tile_extent=source.tile_extent, absolute_roi_extent=256, relative_roi_offset=0
+)
+
+
+@ray.remote
+def handler(slide_path: Path) -> TiledSlideMetadata:
+    slide, tiles = source(slide_path)
+
+    tissue_mask_path = Path(TISSUE_MASKS_PATH, slide_path.name[:-5] + ".tiff")
+    tiles = tissue_mask(tissue_mask_path, slide.extent, tiles)
+
+    return slide, tiles
+
+
+def main() -> None:
+    slides = list(Path(SLIDES_PATH).rglob("*.mrxs"))
+
+    slides, test_slides = train_test_split(slides, test_size=0.2)
+    train_slides, val_slides = train_test_split(slides, test_size=0.1)
+
+    train_slides_df, train_tiles_df = tiling(slides=train_slides, handler=handler)
+    val_slides_df, val_tiles_df = tiling(slides=list(val_slides), handler=handler)
+    test_slides_df, test_tiles_df = tiling(slides=list(test_slides), handler=handler)
+
+    train_slides_df.to_csv("./data/datasets/train_slides.csv", index=False)
+    train_tiles_df.to_csv("./data/datasets/train_tiles.csv", index=False)
+
+    val_slides_df.to_csv("./data/datasets/val_slides.csv", index=False)
+    val_tiles_df.to_csv("./data/datasets/val_tiles.csv", index=False)
+
+    test_slides_df.to_csv("./data/datasets/test_slides.csv", index=False)
+    test_tiles_df.to_csv("./data/datasets/test_tiles.csv", index=False)
+
+    mlflow.set_experiment(experiment_name="Stain-Normalization")
+    with mlflow.start_run(run_name="Stain Normalization Dataset") as _:
+        save_mlflow_dataset(
+            slides=train_slides_df,
+            tiles=train_tiles_df,
+            dataset_name="Stain Normalization - train",
+        )
+        save_mlflow_dataset(
+            slides=val_slides_df,
+            tiles=val_tiles_df,
+            dataset_name="Stain Normalization - val",
+        )
+        save_mlflow_dataset(
+            slides=test_slides_df,
+            tiles=test_tiles_df,
+            dataset_name="Stain Normalization - test",
+        )
+
+
+if __name__ == "__main__":
+    # main()
+    slides = [("/home/jovyan/staining/demo_data/P-2016_0077-08-1_hed_h0.6_e1.5.tiff")]
+    train_slides_df, train_tiles_df = tiling(slides=slides, handler=handler)
+
+    mlflow.set_experiment(experiment_name="Stain-Normalization")
+    with mlflow.start_run(run_name="P-2016_0077-08-1_hed all tissue tiles") as _:
+        save_mlflow_dataset(
+            slides=train_slides_df,
+            tiles=train_tiles_df,
+            dataset_name="P-2016_0077-08-1_hed",
+        )
