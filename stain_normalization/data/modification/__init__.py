6c7ecbfc1b026cac742548e79eb9979e81f684e0
Adam Lopatka <524733@mail.muni.cz>
2026-03-12 14:30:24 +0100

    fix: review ai agent feedback

--- stain_normalization/data/modification/__init__.py
+++ stain_normalization/data/modification/__init__.py
@@ -1,16 +1,16 @@
-from stain_normalization.data.modification.combiend_modification import (
+from stain_normalization.data.modification.combined_modification import (
     CombinedModifications,
 )
 from stain_normalization.data.modification.exposure_adjustment import (
     ExposureAdjustment,
 )
 from stain_normalization.data.modification.hed_factor import HEDFactor
-from stain_normalization.data.modification.hvs_modification import HVSModification
+from stain_normalization.data.modification.hsv_modification import HSVModification
 
 
 __all__ = [
     "CombinedModifications",
     "ExposureAdjustment",
     "HEDFactor",
-    "HVSModification",
+    "HSVModification",
 ]
