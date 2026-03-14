9df92a823dd9e97459880f3fd184183f781bd643
Adam Lopatka <524733@mail.muni.cz>
2026-03-14 14:43:30 +0100

    chore: ruff formating

--- stain_normalization/stain_normalization_model.py
+++ stain_normalization/stain_normalization_model.py
@@ -63,7 +63,9 @@ class StainNormalizationModel(LightningModule):
     def test_step(self, batch: PredictBatch) -> Outputs:
         inputs, data = batch
         outputs = self(inputs)
-        targets = stack([item["original_image_tensor"] for item in data]).to(outputs.device)
+        targets = stack([item["original_image_tensor"] for item in data]).to(
+            outputs.device
+        )
         self.test_metrics.update(outputs, targets)
         self.log_dict(
             self.test_metrics,
